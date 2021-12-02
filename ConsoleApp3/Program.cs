f1c8f8168f0b481438682f3b24a7e61e218e5757
HP OMEN <HP OMEN@DESKTOP-54MF5NR>
2021-12-02 16:45:07 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Enter an amount to convert to coins: ");
+            double response = Convert.ToDouble(Console.ReadLine());
+
+            double goldValue = response / 10;
+            double actualGoldCoins = Math.Floor(response / 10);
+            double remainder = response % 10;
+
+            double silverValue = remainder / 5;
+            double actualSilverCoins = Math.Floor(remainder / 5);
+            double remainder2 = remainder % 5;
+
+            Console.WriteLine($"{response} cents is equal to ...");
+            Console.WriteLine($"Gold coins: {actualGoldCoins}");
+            Console.WriteLine($"Silver coins: {actualSilverCoins}");
+            Console.WriteLine($"Bronse coins: {remainder2}");
+        }
+    }
+}
